765030298ae762dc572cacacfc14568dcd9d7fdf
andresbermeoq <andipandi467@gmail.com>
2021-07-23 02:55:03 -0500

    Create of Components Login and Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteComponent } from './cliente/cliente.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'login', component: ClienteComponent },
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'registrarse', component: RegistrarseComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { ClienteComponent } from './cliente/cliente.component';
+import {MatButtonModule} from '@angular/material/button';
+
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    PedidosComponent,
+    InicioComponent,
+    RegistrarseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
